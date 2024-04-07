a334c4f62f1f16c4030d47c0512bb54de34b1880
vsuminv <81737413+vsuminv@users.noreply.github.com>
2024-04-07 22:03:14 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -3,7 +3,11 @@ def solution(numbers):
     
     for i in range(len(numbers)):
         for j in range(1, len(numbers)):
-            if numbers[i] + numbers[j] not in answer and i != j :
+            # if numbers[i] + numbers[j] not in answer and i != j :
+            #     answer.append(numbers[i] + numbers[j])
+            if i != j:
                 answer.append(numbers[i] + numbers[j])
+    
+    answer= list(set(answer))            
     answer.sort()
     return answer
