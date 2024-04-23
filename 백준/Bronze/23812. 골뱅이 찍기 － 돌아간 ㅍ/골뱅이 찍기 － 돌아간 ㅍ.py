2a88baddc0070c97be9296be3387c525f42d5f6e
vsuminv <81737413+vsuminv@users.noreply.github.com>
2024-04-23 23:26:33 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅍ, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/23812. 골뱅이 찍기 － 돌아간 ㅍ/골뱅이 찍기 － 돌아간 ㅍ.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+for i in range(n):
+    print('@'*n + " "*(3*n) + '@'*n)
+    
+for i in range(n):
+    print('@'*(5*n))
+
+for i in range(n):
+    print('@'*n + " "*(3*n) + '@'*n)
+    
+for i in range(n):
+    print('@'*(5*n))
+
+for i in range(n):
+    print('@'*n + " "*(3*n) + '@'*n)
+    
