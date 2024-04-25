ba4994b1875303e994840ea89abcdb626be69747
vsuminv <81737413+vsuminv@users.noreply.github.com>
2024-04-25 23:40:39 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -0,0 +1,25 @@
+def solution(food):
+    answer = ''
+    cal = 0
+    food_list = []
+    food_list_reverse = []
+    
+    for i in range(1,len(food)):
+        cal = (food[i] //2)
+        food_list += str(i)*cal
+        
+    
+    for i in food_list:
+        if i == 3:
+            food_list_revesr.insert(0,'0')
+        food_list_reverse.append(str(int(i)))
+        food_list_reverse.sort(reverse=True)
+   
+    
+        answer += i 
+       
+    
+        
+        
+        
+    return answer+'0'+''.join(food_list_reverse)
