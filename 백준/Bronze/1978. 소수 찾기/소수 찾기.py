2e9417c9e5846b855a6f099f75ccf49349dda550
vsuminv <81737413+vsuminv@users.noreply.github.com>
2024-05-19 18:17:34 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,15 @@
+n = int(input())
+s = list(map(int,input().split()))
+cnt = 0
+
+for i in s:
+    if i == 1:
+        continue
+    for j in range(2,i):
+        
+        if i % j == 0:
+            break
+    else:
+        cnt += 1
+            
+print(cnt)
