ac3c07850bea82a514030f8c0a330b36640e2380
vsuminv <81737413+vsuminv@users.noreply.github.com>
2024-05-27 00:15:13 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.py
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.py
@@ -1,1 +1,12 @@
-print((2**int(input())+1)**2)
+N = int(input())
+
+for i in range(0,N+1):
+    if i == 0:
+        square = 1
+    else:
+        square *= 2
+
+print((square+1)**2)
+
+# 짧은 정답 코드
+#print((2**int(input())+1)**2)
