99dca7bdea6347aaf88e8b55d0a98b2da853029f
vsuminv <81737413+vsuminv@users.noreply.github.com>
2024-06-29 13:51:08 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 180 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int fibo = sc.nextInt();
+		long[] num_list = new long[fibo+1];
+		num_list[0] = 0;
+		num_list[1] = 1;
+		long result = 0;
+		for (int i = 2; i < num_list.length; i++) {
+			num_list[i] = num_list[i-1] + num_list[i-2];
+		}
+		System.out.println(num_list[fibo]);
+		
+	}
+
+}
