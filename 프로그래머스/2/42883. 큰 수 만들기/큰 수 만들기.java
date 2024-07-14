3483bce09cee7710c01c5a9f27c3735477ec1fbe
vsuminv <81737413+vsuminv@users.noreply.github.com>
2024-07-14 18:04:49 +0900

    [level 2] Title: 큰 수 만들기, Time: 6950.16 ms, Memory: 90.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        int next = 0;
+        
+        for (int i = 0; i < number.length() - k; i++) {
+            int max = 0;
+            
+            for (int j = index; j <= i + k; j++) {
+                int current = number.charAt(j) - '0';
+
+                if (max < current) {
+                    max = current;
+                    next = j;
+                }
+            }
+            sb.append(max);
+            index = next + 1;
+        }
+        return sb.toString();
+    }
+}
