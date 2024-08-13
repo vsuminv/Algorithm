a1a63aaef0849d3c09efa986f4d531afd7c8391a
vsuminv <81737413+vsuminv@users.noreply.github.com>
2024-08-13 17:05:29 +0900

    [level 1] Title: 소수 찾기, Time: 238.83 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int solution(int n) {
+         int answer = 0;
+        
+        for (int i = 2; i <= n; i++) {
+            if (i == 1) {
+                continue;
+            }
+            boolean isPrime = true;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
