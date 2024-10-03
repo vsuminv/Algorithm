f25a4b1bbfa18a5a75b907a0d120597d2e04858e
vsuminv <81737413+vsuminv@users.noreply.github.com>
2024-10-03 23:43:17 +0900

    Update 푸드 파이트 대회.py

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -18,3 +18,20 @@ def solution(food):
         answer += i 
 
     return answer+'0'+''.join(food_list_reverse)
+
+
+# 다른 방법
+def solution(food):
+    answer = ''
+    food_list = []
+    food_list_re=[]
+    cal = 0
+    for i in range(1,len(food)):
+        cal = food[i] // 2
+        food_list.append(str(i)*cal)
+        food_list_re.append(str(i)*cal)
+    
+    food_list_re.reverse()
+    
+        
+    return ''.join(food_list)+"0"+''.join(food_list_re)
