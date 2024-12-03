7cc6a186b3814e75426c43a12eda7d65426dccb6
vsuminv <81737413+vsuminv@users.noreply.github.com>
2024-12-03 23:26:20 +0900

    Update 진법 변환.py

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,3 +1,14 @@
 N,B =input().split()
 B = int(B)
 print(int(N,int(B)))
+
+#방법 2
+N, b = input().split()
+ary = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+N = N[::-1]
+result = 0
+
+for i,n in enumerate(N):
+    result += (int(b)**i)*(ary.index(n))
+print(result)
