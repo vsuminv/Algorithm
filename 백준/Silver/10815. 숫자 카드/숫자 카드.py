7f166003ede4e175e0737b5cc40fdec3ba1b848a
vsuminv <81737413+vsuminv@users.noreply.github.com>
2024-12-09 20:33:10 +0900

    [Silver V] Title: 숫자 카드, Time: 444 ms, Memory: 119332 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,3 +1,7 @@
+# 리스트를 사용해서 조회하면 O(N)
+# set을 해서 조회하면 O(1)이므로 빠르게 조회 가능
+# in 연산자를 사용하면 O(N) 시간이 걸리므로 시간초과 발생하기 때문에 SET 사용
+
 N = int(input())  
 cards = set(map(int, input().split()))
 M = int(input()) 
