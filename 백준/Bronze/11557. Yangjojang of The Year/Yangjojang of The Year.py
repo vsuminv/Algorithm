b5d021838d032a15969d3572a99f4aeed4756772
vsuminv <81737413+vsuminv@users.noreply.github.com>
2024-12-13 21:27:51 +0900

    두 가지 방법

--- 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.py
+++ 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.py
@@ -13,3 +13,19 @@ for i in range(n):
             max_al = al
             max_uni = uni
     print(max_uni)
+
+#딕셔너리 방법
+T = int(input()) 
+
+for _ in range(T):
+    N = int(input())   
+    school_dict = {}  
+
+    for _ in range(N):
+        data = input().split()  
+        school = data[0]  
+        liquor = int(data[1]) 
+        school_dict[school] = liquor  
+
+    max_school = max(school_dict, key=school_dict.get)
+    print(max_school) 
