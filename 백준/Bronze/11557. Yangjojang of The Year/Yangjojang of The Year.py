682fc9af82cb084e1d6217718ba7434d0ce99ea0
vsuminv <81737413+vsuminv@users.noreply.github.com>
2024-12-13 21:26:59 +0900

    [Bronze I] Title: Yangjojang of The Year, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.py
+++ 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.py
@@ -1,14 +1,15 @@
-T = int(input()) 
+n = int(input())
 
-for _ in range(T):
-    N = int(input())   
-    school_dict = {}  
-
-    for _ in range(N):
-        data = input().split()  
-        school = data[0]  
-        liquor = int(data[1]) 
-        school_dict[school] = liquor  
-
-    max_school = max(school_dict, key=school_dict.get)
-    print(max_school) 
+for i in range(n):
+    max_uni = ''
+    max_al = 0
+    num = int(input())
+    for j in range(num):
+        t = input().split()
+        uni = t[0]
+        al = int(t[1])
+        
+        if al > max_al:
+            max_al = al
+            max_uni = uni
+    print(max_uni)
