291ac7302fb89d09d34a9ed97939f25b58ab0c53
vsuminv <81737413+vsuminv@users.noreply.github.com>
2024-12-20 13:28:22 +0900

    [Silver IV] Title: 수 찾기, Time: 152 ms, Memory: 52272 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,11 @@
+N = int(input())
+n_list = set(map(int, input().split()))
+
+M = int(input())
+m_list = list(map(int,input().split()))
+
+for i in m_list:
+    if i in n_list:
+        print(1)
+    else:
+        print(0)
