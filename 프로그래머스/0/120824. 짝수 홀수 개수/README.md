11b0802d735498aee2ffd81e6c8c1071a5d15c26
vsuminv <81737413+vsuminv@users.noreply.github.com>
2026-04-14 09:09:27 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/README.md
+++ 프로그래머스/0/120824. 짝수 홀수 개수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 짝수 홀수 개수 - 120824 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120824?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120824) 
 
 ### 성능 요약
 
-메모리: 9.21 MB, 시간: 0.01 ms
+메모리: 90.1 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 13일 17:16:54
+2026년 04월 14일 09:09:20
 
 ### 문제 설명
 
--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,14 +1,18 @@
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[2];
-        int cnt1 = 0, cnt2 = 0;
-        for (int i = 0; i < num_list.length; i++){
-            if (num_list[i] % 2 == 0) cnt1++;
-            else cnt2++;
+        int odd = 0;
+        int even = 0;
+        for(int i = 0; i < num_list.length; i ++){
+            if (num_list[i] % 2 == 0){
+                even += 1;
+            }
+                
+            else 
+                odd += 1;
         }
-        answer[0] = cnt1;
-        answer[1] = cnt2;
-        
+        answer[0] = even;
+        answer[1] = odd;
         return answer;
     }
 }
