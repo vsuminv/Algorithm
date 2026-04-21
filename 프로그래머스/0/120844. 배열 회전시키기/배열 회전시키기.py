1b167fec19eb613f347f0ca6c7c50848ad33caa3
vsuminv <81737413+vsuminv@users.noreply.github.com>
2026-04-21 16:29:00 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 9.04 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -0,0 +1,12 @@
+def solution(numbers, direction):
+    answer = []
+    
+    if direction == "right":
+        answer.append( numbers[-1])
+        for i in range(0,len(numbers)-1):
+            answer.append(numbers[i])
+        
+    else:
+        answer = numbers[1:]
+        answer.append(numbers[0])
+    return answer
