b1b0d286316e83cf7515d19ddb5a5aea82e4eef6
vsuminv <81737413+vsuminv@users.noreply.github.com>
2026-04-23 10:27:00 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,7 @@
+def solution(my_string):
+    answer = ''
+    my_string= list(my_string)
+    for i in range(len(my_string)):
+        if my_string[i] not in ['a','e','i','o','u']:
+            answer += my_string[i]
+    return answer
